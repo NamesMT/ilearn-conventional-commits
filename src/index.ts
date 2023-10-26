f5a609597447ec3486810a682b12651237dcd74c
Trung Dang <trung.dangquoc@gameloft.com>
2023-10-26 18:21:52 +0000

    feat: add shouldBeNotOK

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import { logger } from '~/logger'
 export const shouldBe200 = OK
 export const fnReturn400 = () => BAD_REQUEST
 export const shouldBeOK = 'OK'
+export const shouldBeNotOK = 'NotOK'
 
 logger.info({ shouldBe200, message: 'hello there' })
