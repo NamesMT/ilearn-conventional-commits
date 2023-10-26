f73e5791aeeaa00f94a89036c6777c91ecc46807
Trung Dang <trung.dangquoc@gameloft.com>
2023-10-26 18:03:19 +0000

    test: add fnReturn400

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,13 @@
 import { expect, it } from 'vitest'
-import { shouldBe200 } from '~/index.js'
+import { fnReturn400, shouldBe200 } from '~/index.js'
 import { logger } from '~/logger'
 
 it('shoudBe200', () => {
   expect(shouldBe200).toBe(200)
+})
+
+it('fnReturn400', () => {
+  expect(fnReturn400()).toBe(400)
 })
 
 it('logger', () => {
