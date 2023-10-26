9d9660b1d12f0e6302c66763cd535cc35509f6bd
Trung Dang <trung.dangquoc@gameloft.com>
2023-10-26 18:02:49 +0000

    feat: add fnReturn400

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { OK } from 'readable-http-codes'
+import { BAD_REQUEST, OK } from 'readable-http-codes'
 import { logger } from '~/logger'
 
 export const shouldBe200 = OK
+export const fnReturn400 = () => BAD_REQUEST
 
 logger.info({ shouldBe200, message: 'hello there' })
